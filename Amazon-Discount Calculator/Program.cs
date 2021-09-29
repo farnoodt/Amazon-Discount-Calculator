b23f411e8d3c0e31d1586704773340e237b42920
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-28 21:06:12 -0700

    Add project files.

---
+++ Amazon-Discount Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Amazon_Discount_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(discount("The price with 25% off is $.99"));
+        }
+
+        static string discount(string s)
+        {
+            string res = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '$')
+                {
+                    string temp = "";
+                    i++;
+                    while (i<s.Length &&((s[i] >= '0' && s[i] <= '9') || s[i] == '.'))
+                    {
+                        temp += s[i];
+                        i++;
+                    }
+                    decimal P = Convert.ToDecimal(temp);
+                    P = P - (P / 4);
+                    res += P.ToString("C2");
+                }
+                else
+                    res += s[i];
+            }
+            return res;
+        }
+    }
+}
